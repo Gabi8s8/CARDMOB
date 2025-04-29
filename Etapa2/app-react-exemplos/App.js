b88452b9d751dd4fe12c450ab8da57cbd029c8ad
Gabriela Teixeira <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-04-29 08:59:35 -0300

    Exemplo: contador + imagem

--- Etapa2/app-react-exemplos/App.js
+++ Etapa2/app-react-exemplos/App.js
@@ -1,20 +1,60 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1);
+  }
+
+  const decrementCounter = () => {
+    setCounter(counter - 1);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+          <Text style={styles.text}>Hello World!</Text>
+          <Image
+              source={{ uri: 'https://picsum.photos/200' }}
+              style={{ width: 200, height: 200 }}
+          />
+
+          <StatusBar style="auto" />
+          <Text style={styles.text}>Counter: {counter}</Text>
+
+          <View style={styles.buttonContainer}>
+              <Button
+                  title="Increment"
+                  onPress={incrementCounter}
+              />
+              <Button
+                  title="Decrement"
+                  onPress={decrementCounter}
+              />
+          </View>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        fontFamily: 'cursive',
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      gap: 10,
+    },
 });
+
+// onPress = OnClick
