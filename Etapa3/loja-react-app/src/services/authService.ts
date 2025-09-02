9026bcbe5a5c04855668a07e3fc956dd68398769
Gabriela <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-09-02 09:27:24 -0300

    Login com backend

--- Etapa3/loja-react-app/src/services/authService.ts
+++ Etapa3/loja-react-app/src/services/authService.ts
@@ -1,6 +1,57 @@
-export async function fakelogin(email: string, password: string): Promise<string> {
-    if (email === "text@example.com" && password === '123') {
+import Config from 'react-native-config';
+
+// const API_URL = Config.API_URL;
+const API_URL = 'http://10.81.205.28:5000';
+
+export async function fakelogin(
+    email: string,
+    password: string
+): Promise<string> {
+    if (email === 'text@example.com' && password === '123') {
         return Promise.resolve('fake-jwt-token');
     }
+    if (email === 'admin@example.com' && password === 'admin123') {
+        try {
+            const response = await fetch(`${API_URL}/users/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await response.json();
+            const jwt = data.accessToken;
+            console.log(jwt);
+            return Promise.resolve(jwt);
+        } catch (error) {
+            console.error('Crednciais inválidas');
+            return Promise.reject(error);
+        }
+    }
+
     return Promise.reject('Credenciais inválidas');
+}
+
+export async function requestLogin(
+    email: string,
+    password: string
+): Promise<string> {
+    console.log(API_URL);
+    try {
+        const response = await fetch(`${API_URL}/api/users/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const data = await response.json();
+        const jwt = data.accessToken;
+        console.log(jwt);
+        return Promise.resolve(jwt);
+    } catch (error) {
+        console.error(error);
+        return Promise.reject(error);
+        // return Promise.reject('Credenciais inválidas');
+    }
 }
