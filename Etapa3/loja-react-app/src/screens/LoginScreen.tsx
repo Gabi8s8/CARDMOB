af06b589a4db343976ded29cc5fe0ef79b2d55d9
Gabriela <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-09-02 08:01:51 -0300

    Inclui exemplo e icone no TabNavigator

--- Etapa3/loja-react-app/src/screens/LoginScreen.tsx
+++ Etapa3/loja-react-app/src/screens/LoginScreen.tsx
@@ -1,55 +1,61 @@
-import React, { useState } from "react";
-import { View, TextInput, Button, StyleSheet, Text} from "react-native";
+import React, { useState } from 'react';
+import {
+    View,
+    TextInput,
+    Button,
+    StyleSheet,
+    Text,
+    SafeAreaView,
+} from 'react-native';
 
-import { fakelogin } from "../services/authService";
-import { useAuth } from "../contexts/AuthContext"
+import { fakelogin } from '../services/authService';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function LoginScreen({ navigation }: any) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const { login } = useAuth()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const { login } = useAuth();
 
     const handleLogin = async () => {
         try {
             // Lógica de login / conexão com backend
-            const token = await fakelogin(email, password)
-            login(token)
-            console.log("Login ok");
+            const token = await fakelogin(email, password);
+            login(token);
+            console.log('Login ok');
         } catch (err: any) {
             setError(err);
         }
-    }
+    };
 
     return (
-        <View style={styles.container}>
-            <Text>Email:</Text>
-            <TextInput
-                style={styles.input}
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-            />
-            <Text>Senha:</Text>
-            <TextInput
-                style={styles.input}
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry // para ocultar a senha
-            />
+        <SafeAreaView style={styles.container}>
+            <View>
+                <Text>Email:</Text>
+                <TextInput
+                    style={styles.input}
+                    value={email}
+                    onChangeText={setEmail}
+                    autoCapitalize="none"
+                />
+                <Text>Senha:</Text>
+                <TextInput
+                    style={styles.input}
+                    value={password}
+                    onChangeText={setPassword}
+                    secureTextEntry // para ocultar a senha
+                />
 
-            { error ? 
-                <Text
-                    style={{ color: "red" }}
-                >{error}
-                </Text> : 
-                null 
-            }
+                {error ? <Text style={{ color: 'red' }}>{error}</Text> : null}
 
-            <Button title="Entrar" onPress={handleLogin} />
-            <Button title="Registrar" onPress={() => navigation.navigate("Register")}/>
-        </View>
-    )
+                <Button title="Entrar" onPress={handleLogin} />
+                <Button
+                    title="Registrar"
+                    onPress={() => navigation.navigate('Register')}
+                />
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -63,5 +69,5 @@ const styles = StyleSheet.create({
         borderColor: '#ccc',
         padding: 8,
         marginBottom: 12,
-    }
-})
+    },
+});
