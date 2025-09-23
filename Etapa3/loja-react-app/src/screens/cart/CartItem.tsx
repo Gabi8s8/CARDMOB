6fc46644dd6f1a951d70f7002e9d0cac5e0c22b9
Gabriela <gabriela.teixeira1@aluno.ifsp.edu.br>
2025-09-23 08:19:10 -0300

    carrinho de compras mínimo

---
+++ Etapa3/loja-react-app/src/screens/cart/CartItem.tsx
@@ -0,0 +1,66 @@
+import React, { useContext } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
+
+const CartItem = ({ item }: any) => {
+    //Para fazer: implementar o context para persistir dados do carrinho
+
+    const handleRemove = () => {
+        console.log('item excluído');
+    };
+
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: item.image }} style={styles.image} />
+            <View>
+                <Text style={styles.name}>{item.name}</Text>
+                <View style={styles.quantity}>
+                    <Text style={styles.price}>
+                        R$ {(item.price * item.quantity).toFixed(2)}
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => handleRemove()}
+                        style={styles.button}
+                    >
+                        <Text style={StyleSheet.buttonText}>-</Text>
+                    </TouchableOpacity>
+                    <Text style={styles.quantityValue}>{item.quantity}</Text>
+                    <TouchableOpacity
+                        onPress={() => handleRemove()}
+                        style={styles.button}
+                    >
+                        <Text style={StyleSheet.buttonText}>+</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => handleRemove()}
+                        style={styles.button}
+                    >
+                        <Text style={StyleSheet.buttonText}>Remover</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </View>
+    );
+};
+export default CartItem;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd',
+    },
+    image: {
+        width: '50%',
+        height: 100,
+        marginRight: 8,
+        borderColor: '#ddd',
+    },
+    name: {},
+    quantity: {},
+    price: {},
+    button: {},
+    quantityValue: {},
+    buttonText: {},
+})
